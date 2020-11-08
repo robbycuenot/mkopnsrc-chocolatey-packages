30fbdfda2df2c09b5275d92044201724847f3869
Mahesh K <kakadiam@gmail.com>
2020-11-08 01:03:25 -0500

    new packetbeat-oss package initial with update script

---
+++ automatic/packetbeat-oss/tools/chocolateyInstall.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+	packageName     = $env:ChocolateyPackageName
+	softwareName    = 'Beats packetbeat-oss*'
+  version         = $env:ChocolateyPackageVersion
+	unzipLocation   = $toolsDir
+	installerType   = 'msi'
+	url             = 'https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-oss-7.9.3-windows-x86.msi'
+	url64bit        = 'https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-oss-7.9.3-windows-x86_64.msi'
+	checksum        = '6117f0ea208c625f4456b8dba238b63cee1f738217083b4df8c707117fb6ead9'
+	checksumType    = 'SHA256'
+	checksum64      = '841b9a06d3d54d9e39351dab736479b9f7f17752c50ffbdbb488789a600250da'
+	checksumType64  = 'SHA256'
+	silentArgs      = "/qn /norestart"
+	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+  validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    2147205120  # pending restart required for setup update
+  )
+}
+
+$alreadyInstalled = (AlreadyInstalled -AppName $packageArgs['softwareName'] -AppVersion $packageArgs['version'])
+
+if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
+  Write-Output $(
+    $packageArgs['softwareName']+" is already installed. " +
+    'if you want to re-install, use "--force" option to re-install.'
+  )
+} else {
+	Install-ChocolateyPackage @packageArgs
+  
+  Write-Output "
+  ###################### Packetbeat Configuration ########################
+    Packetbeat Config File Location: 
+    
+    $env:programdata\Elastic\Beats\packetbeat\packetbeat.yml
+    
+    You can find the full configuration reference here:
+    https://www.elastic.co/guide/en/beats/packetbeat/current/index.html
+  ###################### ######################## ########################
+  "
+}
