aecf323f0aa0bf3734a577f61276dfeab2076982
Mahesh K <kakadiam@gmail.com>
2020-11-07 20:57:24 -0500

    winlogbeat-oss initial package

---
+++ automatic/winlogbeat-oss/tools/chocolateyInstall.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+	packageName   = $env:ChocolateyPackageName
+	softwareName  = 'Beats winlogbeat-oss*'
+  version       = $env:ChocolateyPackageVersion
+	unzipLocation = $toolsDir
+	installerType = 'msi'
+	url           = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-oss-7.9.3-windows-x86.msi'
+	url64bit      = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbeat-oss-7.9.3-windows-x86_64.msi'
+	checksum      = '1fbfd9941218c9dac096b6ee9e7db520d6912d579454f94c6da653879f6dbb51'
+	checksumType  = 'SHA256' #default is md5, can also be sha1, sha256 or sha512
+	checksum64    = 'e101c28ac56d64d3fde42b6dd377281614f15baade4e498b3e00c11f0fdeb850'
+	checksumType64= 'SHA256' #default is checksumType
+	silentArgs = "/qn /norestart"
+	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+  validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    2147205120  # pending restart required for setup update
+  )
+}
+
+$alreadyInstalled = (AlreadyInstalled -AppName $packageArgs['softwareName'] -AppVersion $packageArgs['version'])
+
+if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
+  Write-Output $(
+    $packageArgs['softwareName']+" is already installed. " +
+    'if you want to re-install, use "--force" option to re-install.'
+  )
+} else {
+	Install-ChocolateyPackage @packageArgs
+  
+  Write-Output "
+  ###################### Winlogbeat Configuration ########################
+    Winlogbeat Config File Location: 
+    
+    $env:programdata\Elastic\Beats\winlogbeat\winlogbeat.yml
+    
+    You can find the full configuration reference here:
+    https://www.elastic.co/guide/en/beats/winlogbeat/index.html
+  ###################### ######################## ########################
+  "
+}
---
+++ automatic/winlogbeat-oss/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,10 @@
+﻿$WinPackageName = 'Beats winlogbeat-oss'
+$processName = 'winlogbeat'
+$Proc = @("$processName")
+$ProcActive = Get-Process -Name $Proc -ErrorAction SilentlyContinue
+
+## Stop winlogbeat application if it is already running
+if($ProcActive){
+	Write-Warning "Application $processName is currently running, the service will be stopped now."
+	Stop-Service $processName -Force -ErrorAction SilentlyContinue
+}
