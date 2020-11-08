4d77151fd8e602a1a8a54509cfd1b0e972b2916b
Mahesh K <kakadiam@gmail.com>
2020-11-07 20:55:07 -0500

    filebeat-oss package updates

--- automatic/filebeat-oss/tools/chocolateyInstall.ps1
+++ automatic/filebeat-oss/tools/chocolateyInstall.ps1
@@ -2,24 +2,19 @@
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 . $toolsDir\helpers.ps1
 
-# 32bit installer
-$url = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.9.3-windows-x86.msi'
-# 64bit installer
-$url64 = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.9.3-windows-x86_64.msi'
-
 $packageArgs = @{
-	packageName   = $env:ChocolateyPackageName
-	softwareName  = 'Beats filebeat-oss*'
-  version       = $env:ChocolateyPackageVersion
-	unzipLocation = $toolsDir
-	installerType = 'msi'
-	url           = $url
-	url64bit      = $url64
-	checksum      = '15d0995813d0e6d02c0d63470d770577f23c8164da20fe234fde4ad672a4512f809f228678398cfc29a1fc5efc21c9363f69de0d2256269b0838302030cdd1e9'
-	checksumType  = 'sha512' #default is md5, can also be sha1, sha256 or sha512
-	checksum64    = '822ee916e0d8284a50a836214ee4c26adc04c4edba136a0d810b6461ec474d0b343976f4df72abad90758f6187cadf4e19fd265bb68c5d76e1c075b961ac6086'
-	checksumType64= 'sha512' #default is checksumType
-	silentArgs = "/qn /norestart"
+	packageName     = $env:ChocolateyPackageName
+	softwareName    = 'Beats filebeat-oss*'
+  version         = $env:ChocolateyPackageVersion
+	unzipLocation   = $toolsDir
+	installerType   = 'msi'
+	url             = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.9.3-windows-x86.msi'
+	url64bit        = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.9.3-windows-x86_64.msi'
+	checksum        = '83a3f6323da472f3d769944a5211174ceb7e200de0016c43410126e304eb367f'
+	checksumType    = 'SHA256'
+	checksum64      = '62bef33fb923b9e707c6f2bfbaa876a72b3a417eade44673d761a9df38c4819e'
+	checksumType64  = 'SHA256'
+	silentArgs      = "/qn /norestart"
 	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
   validExitCodes = @(
     0, # success
