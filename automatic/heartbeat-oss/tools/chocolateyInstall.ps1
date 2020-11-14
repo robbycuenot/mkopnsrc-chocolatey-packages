62224d428c2441046ff7defdbcdfd9f2538bbc6d
Mahesh K <kakadiam@gmail.com>
2020-11-14 14:27:28 -0500

    new heartbeat-oss package with update script

---
+++ automatic/heartbeat-oss/tools/chocolateyInstall.ps1
@@ -0,0 +1,47 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+	packageName     = $env:ChocolateyPackageName
+	softwareName    = "Beats $($env:ChocolateyPackageName)*"
+  version         = $env:ChocolateyPackageVersion
+	unzipLocation   = $toolsDir
+	installerType   = 'msi'
+	url             = 'https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-oss-7.10.0-windows-x86.msi'
+	url64bit        = 'https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-oss-7.10.0-windows-x86_64.msi'
+	checksum        = '06f2d7d7c2c07035e440282ae1d8d67cca1eecdd04473e97b4b2383c810b8ede'
+	checksumType    = 'SHA256'
+	checksum64      = '05564586902a265c0f08edc121f1536fba352eb2e489243daaf1d9886aaa4aae'
+	checksumType64  = 'SHA256'
+	silentArgs      = "/qn /norestart"
+	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+  validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    2147205120  # pending restart required for setup update
+  )
+}
+
+$alreadyInstalled = (AlreadyInstalled -AppName $packageArgs['softwareName'] -AppVersion $packageArgs['version'])
+
+if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
+  Write-Output $(
+    $packageArgs['softwareName']+" is already installed. " +
+    'if you want to re-install, use "--force" option to re-install.'
+  )
+} else {
+	Install-ChocolateyPackage @packageArgs
+  
+  Write-Output "
+  ###################### Heartbeat Configuration ########################
+  Heartbeat Config File Location: 
+    
+    $env:programdata\Elastic\Beats\heartbeat\heartbeat.yml
+    
+    You can find the full configuration reference here:
+    https://www.elastic.co/guide/en/beats/heartbeat/current/index.html
+  ###################### ######################## ########################
+  "
+}
---
+++ automatic/heartbeat-oss/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,8 @@
+﻿$processName = 'heartbeat'
+$Proc = @("$processName")
+$ProcActive = Get-Process -Name $Proc -ErrorAction SilentlyContinue
+
+if($ProcActive){
+	Write-Warning "Application $processName is currently running, the service will be stopped now."
+	Stop-Service $processName -Force -ErrorAction SilentlyContinue
+}
