e7d751d757e80c716094ae97d756110806ebd42f
Mahesh K <kakadiam@gmail.com>
2020-11-17 22:14:03 -0500

    new package pgptool with update script

---
+++ automatic/pgptool/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+	packageName     = $env:ChocolateyPackageName
+	softwareName    = 'PGP Tool*'
+  version         = $env:ChocolateyPackageVersion
+	unzipLocation   = $toolsDir
+	installerType   = 'msi'
+	url             = 'https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-oss-7.10.0-windows-x86.msi'
+	checksum        = '379b79cf88bbe354e12a739aadbee34340dd566f6660d41734ca7fd3c792cc1f'
+	checksumType    = 'SHA256'
+	silentArgs      = "/qn /norestart"
+	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+  validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    2147205120  # pending restart required for setup update
+  )
+}
+
+$alreadyInstalled = (AlreadyInstalled -AppName $packageArgs['softwareName'] -AppVersion $packageArgs['version'])
+
+if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
+  Write-Output $(
+    $packageArgs['softwareName']+" is already installed. " +
+    'if you want to re-install, use "--force" option to re-install.'
+  )
+} else {
+	Install-ChocolateyPackage @packageArgs
+}
