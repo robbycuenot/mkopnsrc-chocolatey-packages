80e26cb8f97d8d50da92198bec6236d35edb3dd1
Mahesh K <kakadiam@gmail.com>
2021-08-19 14:35:06 -0400

    new choco package - meshcommander

---
+++ automatic/meshcommander/tools/chocolateyInstall.ps1
@@ -0,0 +1,37 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+	packageName     = $env:ChocolateyPackageName
+	softwareName    = 'MeshCommander*'
+  version         = $env:ChocolateyPackageVersion
+	unzipLocation   = $toolsDir
+	installerType   = 'msi'
+	url             = 'https://info.meshcentral.com/downloads/mdtk/MeshCommander.msi'
+	#url64bit        = ''
+	checksum        = '5ccbce0486346840828c25704d66f7caa2f51d6f7f3edb9c5a6b4f13a049e9cd'
+	checksumType    = 'SHA256'
+	#checksum64      = ''
+	#checksumType64  = 'SHA256'
+	silentArgs      = "/qn /norestart"
+	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+  validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    2147205120  # pending restart required for setup update
+  )
+}
+
+$alreadyInstalled = (AlreadyInstalled -AppName $packageArgs['softwareName'] -AppVersion $packageArgs['version'])
+
+if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
+  Write-Output $(
+    $packageArgs['softwareName']+" is already installed. " +
+    'if you want to re-install, use "--force" option to re-install.'
+  )
+} else {
+	Install-ChocolateyPackage @packageArgs
+
+}
---
+++ automatic/meshcommander/update.ps1
@@ -0,0 +1,43 @@
+Import-Module au
+
+$releases = 'https://www.meshcommander.com/meshcommander'
+$ChecksumType = 'SHA256'
+function global:au_BeforeUpdate {
+    $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32 -Algorithm $Latest.ChecksumType32
+    #$Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64 -Algorithm $Latest.ChecksumType64
+
+    #AU function Get-RemoteFiles can download files and save them in the package's tools directory. It does that by using the $Latest.URL32 and/or $Latest.URL64.
+    #Get-RemoteFiles -Purge
+}
+function global:au_GetLatest() {
+    $download_page = Invoke-WebRequest $releases -UseBasicParsing
+    $url_obj   = $download_page.links | Where-Object href -match '.msi'
+    #$url64 = $download_page.links | ? href -match '\windows-x86_64.msi$' | select -First 1 -expand href
+    $url = (Invoke-WebRequest $url_obj.href).Links |Select-Object -First 1 -ExpandProperty href
+    $version = (($url_obj.outerHTML) |Select-String \d.\d.\d).Matches.Value
+    $ext = $url.Split('.') |Select-Object -Last 1
+    $Latest = @{
+        InstallerType = $ext
+        URL32     = $url
+        #URL64     = $url64
+        Version   = $version
+        ChecksumType32 = $ChecksumType
+        #ChecksumType64 = $ChecksumType
+    }
+    return $Latest
+}
+function global:au_SearchReplace {
+    @{
+      ".\tools\chocolateyInstall.ps1" = @{
+        "(?i)(^\s*installerType\s*=\s*)('.*')" = "`$1'$($Latest.InstallerType)'"
+        "(?i)(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
+        #"(?i)(^\s*url64bit\s*=\s*)('.*')" = "`$1'$($Latest.URL64)'"
+        "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+        #"(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        #"(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
+      };
+
+    }
+}
+update -ChecksumFor none #-NoCheckChocoVersion
