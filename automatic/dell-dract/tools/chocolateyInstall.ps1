544fdc2c81f2c4d33841e2d6ee11e5afd262c367
Mahesh K <kakadiam@gmail.com>
2022-02-08 16:49:02 -0500

    added new dell-dract package

---
+++ automatic/dell-dract/tools/chocolateyInstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+	packageName     = $env:ChocolateyPackageName
+	softwareName    = 'Dell Remote Access Configuration Tool*'
+  	version         = $env:ChocolateyPackageVersion
+	unzipLocation   = $toolsDir
+	installerType   = 'msi'
+	url             = 'https://dl.dell.com/FOLDER02535896M/1/RACT.msi'
+	#url64bit        = ''
+	checksum        = '7347cb77f08fd7adbe209193bd1367d3875e670c58cae1e1612b6a284792db3e'
+	checksumType    = 'SHA256'
+	#checksum64      = ''
+	#checksumType64  = 'SHA256'
+	silentArgs      = "/qn /norestart"
+	#Exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+  validExitCodes = @(
+    0, # success
+    3010, # success, restart required
+    2147781575, # pending restart required
+    2147205120  # pending restart required for setup update
+  )
+}
+
+$alreadyInstalled = (AlreadyInstalled -AppName $packageArgs['softwareName'] -AppVersion $packageArgs['version'])
+
+if ($alreadyInstalled -and ($env:ChocolateyForce -ne $true)) {
+  Write-Output $(
+    $packageArgs['softwareName']+" is already installed. " +
+    'if you want to re-install, use "--force" option to re-install.'
+  )
+} else {
+	Install-ChocolateyPackage @packageArgs
+}
